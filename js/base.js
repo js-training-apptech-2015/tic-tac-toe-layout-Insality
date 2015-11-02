73ca663e2516fa665768cd4f633ea4ae85fbd8dc
Insality <insality@gmail.com>
2015-11-02 22:53:33 +0600

    TicTac ready

--- js/base.js
+++ js/base.js
@@ -1,3 +1,26 @@
 ;(function(){
 	console.log("Hello!");
-});
+
+	$(".tile").click(function(){
+		var ch = $(this).text();
+		var replaceCh = "";
+		var tileClass = "";
+
+		if (ch === "•"){
+			replaceCh = "O";
+			tileClass = "circle";
+		} 
+		if (ch === "O"){
+			replaceCh = "X";
+			tileClass = "cross";
+		} 
+		if (ch === "X"){
+			replaceCh = "•";
+		} 
+
+		$(this).text(replaceCh);
+		$(this).removeClass("circle");
+		$(this).removeClass("cross");
+		$(this).addClass(tileClass);
+	});
+}());
